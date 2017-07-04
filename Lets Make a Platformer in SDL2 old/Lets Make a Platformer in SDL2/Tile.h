e4613d66b9b37100840aa25a79c1536c9bb26267
shout12341312312 <sc5shout@gmail.com>
2017-07-04 16:31:23 +0200

    Add files via upload

---
+++ Lets Make a Platformer in SDL2 old/Lets Make a Platformer in SDL2/Tile.h
@@ -0,0 +1,32 @@
+﻿/*Written 2017 by Michał Przekota.
+To the extent possible under law, the author(s) have dedicated all copyright to men who want to help with the problem.
+*/
+#pragma once
+
+#include <SDL.h>
+#include <iostream>
+#include "Texture.h"
+#include "Animation.h"
+#include "Player.h"
+
+class Tile
+{
+public:
+	Tile();
+	~Tile();
+
+	static enum State { Solid, Passive };
+
+	void SetContent(std::string image, State state, float posX, float posY, SDL_Renderer *renderer, SDL_Rect rect);
+	void UnloadContent();
+	void Update(Player &e);
+	void Draw(SDL_Renderer *renderer);
+
+private:
+	Animation anim;
+	State state;
+	SDL_Rect rect;
+
+	float posX, posY;
+};
+
